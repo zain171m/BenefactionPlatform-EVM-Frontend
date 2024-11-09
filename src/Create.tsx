a1e4f5f34df67b574630d2851721ec9d383778d7
zain171m <zain171m@gmail.com>
2024-11-09 21:19:49 +0500

    Designed & Optimized the form to create funding Vault used react-hook-form, Made changes to the home file.

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,16 +1,92 @@
 //import abi from "./abi/abi.json";
-//import { useWriteContract } from "wagmi";
+import { useWriteContract } from "wagmi";
 //import { parseEther } from "viem";
+import { useForm, SubmitHandler } from "react-hook-form";
+import factoryabi from "./abi/factoryabi.json";
+import { arbitrumSepolia } from "viem/chains";
+
+type Inputs = {
+  title: string;
+  url: string;
+  description: string;
+  pta: string;
+  withdrawAddress: string;
+  developerAddress: string;
+  minEth: string;
+  deadline: Date;
+  ptaAmount: string;
+  rate: string;
+  developerPercentage: string;
+};
 
 const Create = () => {
-  //const { writeContractAsync } = useWriteContract();
-
+  const { writeContractAsync } = useWriteContract();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data);
+    //const timestamp = Math.floor(data.deadline.getTime() / 1000);
+    try {
+      const tx = await writeContractAsync({
+        abi: factoryabi,
+        address: "0x3d927Cf8E568856e379FDAaaa131d16aCa652e6",
+        functionName: "deployFundingVault",
+        args: [
+          data.pta,
+          data.ptaAmount,
+          data.minEth,
+          data.deadline,
+          data.rate,
+          data.withdrawAddress,
+          data.developerAddress,
+          data.developerPercentage,
+          data.url,
+          data.title,
+          data.description,
+        ],
+        chainId: arbitrumSepolia.id,
+      });
+      console.log("Transaction submitted:", tx);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+    //  try {
+    //   const tx = await writeAsync();
+    //   console.log('Transaction submitted:', tx.hash);
+    //   const receipt = await tx.wait();
+    //   console.log('Transaction confirmed:', receipt);
+    // } catch (error) {
+    //   console.error('Transaction failed:', error);
+    // }
+  };
+  //console.log(watch("title")); // watch input value by passing the name of it
   return (
+    // <form
+    //   className="text-black flex-row gap-3"
+    //   onSubmit={handleSubmit(onSubmit)}
+    // >
+    //   {/* register your input into the hook by invoking the "register" function */}
+    //   <input defaultValue="test" {...register("example")} />
+
+    //   {/* include validation with required or other standard HTML validation rules */}
+    //   <input {...register("exampleRequired", { required: true })} />
+    //   {/* errors will return when field validation fails  */}
+    //   {errors.exampleRequired && (
+    //     <span className="text-white">This field is required</span>
+    //   )}
+
+    //   <input type="submit" />
+    // </form>
     <div className="mx-auto max-w-7xl p-5">
       <div className="py-3 flex flex-col gap-2">
-        <h1 className="text-2xl text-white">Create new campaign</h1>
+        <h1 className="text-2xl text-white">Create new Funding Vault</h1>
       </div>
       <form
+        onSubmit={handleSubmit(onSubmit)}
         //   onSubmit={async (e) => {
         //   e.preventDefault();
         //   const formObj = new FormData(e.currentTarget);
@@ -59,12 +135,12 @@ const Create = () => {
           /> */}
 
           <div>
-            <label className={`text-sm text-slate-400`}>Name</label>
+            <label className={`text-sm text-slate-400`}>Project Name</label>
             <input
-              id="name"
-              name="name"
-              className="bg-transparent p-2 text-white text-sm w-full outline-none border border-slate-600 rounded-md"
+              id="title"
+              className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
               required
+              {...register("title", { required: true })}
             />
           </div>
           {/* <Input
@@ -80,12 +156,12 @@ const Create = () => {
             }}
           /> */}
           <div>
-            <label className={`text-sm text-slate-400`}>Title</label>
+            <label className={`text-sm text-slate-400`}>Project URL</label>
             <input
-              id="title"
-              name="title"
+              id="url"
               className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
               required
+              {...register("url", { required: true })}
             />
           </div>
         </div>
@@ -105,11 +181,11 @@ const Create = () => {
         <label className={`text-sm text-slate-400`}>Description</label>
         <textarea
           id="description"
-          name="description"
           className="bg-transparent p-5 text-sm w-full outline-none border border-slate-600 h-40 rounded-md"
-          required
+          {...register("description", { required: true })}
         />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 pt-5 pb-10">
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-5">
           {/* <Input
             label={"Target"}
             type={"number"}
@@ -121,13 +197,72 @@ const Create = () => {
             validation={{}}
           /> */}
           <div>
-            <label className={`text-sm text-slate-400`}>Target</label>
+            <label className={`text-sm text-slate-400`}>
+              Partcipation Token Address
+            </label>
             <input
-              id="target"
-              name="target"
+              id="pta"
+              className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
+              {...register("pta", { required: true })}
+            />
+          </div>
+          {/* <Input
+            label={"Deadline"}
+            type={"date"}
+            register={register}
+            errors={errors}
+            name="deadline"
+            validation={{}}
+          /> */}
+          <div>
+            <label className={`text-sm text-slate-400`}>
+              Withdrawl Address
+            </label>
+            <input
+              id="withdrawAddress"
+              className="bg-transparent text-white p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
+              {...register("withdrawAddress", { required: true })}
+            />
+          </div>
+          {/* <Input
+            label={"Image Link"}
+            type={"url"}
+            register={register}
+            errors={errors}
+            name="image"
+            validation={{}}
+          /> */}
+          <div>
+            <label className={`text-sm text-slate-400`}>
+              Developer Fee Address
+            </label>
+            <input
+              id="developerAddress"
+              className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
+              {...register("developerAddress", { required: true })}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-5">
+          {/* <Input
+            label={"Target"}
+            type={"number"}
+            step={"0.1"}
+            min={0.1}
+            register={register}
+            errors={errors}
+            name="target"
+            validation={{}}
+          /> */}
+          <div>
+            <label className={`text-sm text-slate-400`}>
+              Minimum ETH Target
+            </label>
+            <input
+              id="minEth"
               type="number"
               className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
-              required
+              {...register("minEth", { required: true })}
             />
           </div>
           {/* <Input
@@ -142,10 +277,9 @@ const Create = () => {
             <label className={`text-sm text-slate-400`}>Deadline</label>
             <input
               id="deadline"
-              name="deadline"
               type="date"
               className="bg-transparent text-white p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
-              required
+              {...register("deadline", { required: true })}
             />
           </div>
           {/* <Input
@@ -157,18 +291,66 @@ const Create = () => {
             validation={{}}
           /> */}
           <div>
-            <label className={`text-sm text-slate-400`}>Image Link</label>
+            <label className={`text-sm text-slate-400`}>
+              Participation Token Amount
+            </label>
             <input
-              id="image"
-              name="image"
-              type="url"
+              id="ptaAmount"
+              type="number"
               className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
-              required
+              {...register("ptaAmount", { required: true })}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 py-5">
+          {/* <Input
+            label={"Name"}
+            register={register}
+            errors={errors}
+            name="name"
+            validation={{
+              minLength: {
+                value: 3,
+                message: "Please enter at least 3 characters",
+              },
+            }}
+          /> */}
+
+          <div>
+            <label className={`text-sm text-slate-400`}>Exchange Rate</label>
+            <input
+              id="rate"
+              type="number"
+              className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
+              {...register("rate", { required: true })}
+            />
+          </div>
+          {/* <Input
+            label={"Title"}
+            register={register}
+            errors={errors}
+            name="title"
+            validation={{
+              minLength: {
+                value: 10,
+                message: "Please enter at least 10 characters",
+              },
+            }}
+          /> */}
+          <div>
+            <label className={`text-sm text-slate-400`}>
+              Developer Fee percentage
+            </label>
+            <input
+              id="developerPercentage"
+              className="bg-transparent p-2 text-sm w-full outline-none border border-slate-600 rounded-md"
+              {...register("developerPercentage", { required: true })}
             />
           </div>
         </div>
         <button
           type="submit"
+          disabled={isSubmitting}
           className="text-white bg-slate-800 font-bold text-base h-10 hover:bg-purple-800 px-3 rounded-lg shadow-md"
         >
           Create Campaing
@@ -177,5 +359,4 @@ const Create = () => {
     </div>
   );
 };
-
 export default Create;
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,59 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { VaultArrayType } from "./ContractResponseTypes.ts";
 import factoryabi from "./abi/factoryabi.json";
 import { useReadContract } from "wagmi";
 //import abi from "./abi/abi.json";
 import { arbitrumSepolia } from "viem/chains";
 const Home = () => {
-  const [start] = useState(1);
-  const [end] = useState(1);
-  // const {
-  //   data: vaults,
-  //   isLoading,
-  //   isError,
-  //   error,
-  // } = useReadContract<VaultArrayType>({
-  //   address: "0x8b48094F9D18557a57986A41124F673673909773",
-  //   abi: factoryabi,
-  //   functionName: "getVaults",
-  //   args: [start, end],
-  //   watch: true, // Automatically re-fetch when contract state changes
-  // });
+  const [totalVaults, setTotalVaults] = useState<number>();
+  const [start, setStart] = useState<number>();
+  const [end, setEnd] = useState<number>();
 
-  // if (isLoading) return <p>Loading...</p>;
-  // if (isError) return <p>Error: {error.message}</p>;
+  // First contract call
+  const response = useReadContract({
+    abi: factoryabi,
+    address: "0x3d927Cf8E568856e379FDAaaa131d16aCa652e6d",
+    functionName: "getTotalNumberOfFundingVaults",
+    chainId: arbitrumSepolia.id,
+  });
 
+  // Update totalVaults, start, and end only when `response` fetches new data
+  useEffect(() => {
+    if (response.isFetched) {
+      const vaultCount = Number(response?.data);
+      setTotalVaults(vaultCount);
+      setStart(Math.max(vaultCount - 10, 1));
+      setEnd(vaultCount);
+    }
+  }, [response.isFetched, response?.data]);
+
+  // Second contract call, triggered only when `totalVaults`, `start`, and `end` are defined
   const result = useReadContract({
     abi: factoryabi,
     address: "0x3d927Cf8E568856e379FDAaaa131d16aCa652e6d",
     functionName: "getVaults",
-    args: [start, end],
+    args: start && end ? [start, end] : undefined, // Only provide args when start and end are set
     chainId: arbitrumSepolia.id,
+    query: {
+      enabled:
+        totalVaults !== undefined && start !== undefined && end !== undefined,
+    },
   });
+
   const vaults = result?.data as VaultArrayType;
-  // if (result.isError) {
-  //   return <div>error occured</div>;
-  // }
-  //console.log(vaults[0].deadline);
+
   return (
     <div id="projects" className="px-4 xl:px-28 lg:px-20 sm:px-8">
       <h1 className="text-3xl pt-16 font-bold text-white">
         All Funding Vaults
       </h1>
-      <p className="text-slate-200 pt-3 pb-8">
+      <p className="text-slate-200 pt-3 pb-4">
         Each Funding Vault represents a funding project.
       </p>
 
-      <div className=" md:grid grid-cols-3 gap-6">
-        {result.isLoading &&
+      <div className="md:grid grid-cols-3 gap-6 py-4">
+        {!result.isFetched &&
           Array.from({ length: 3 }, (_, index) => (
             <div key={index}>
               <div className="px-7 py-7 bg-slate-950 rounded-lg shadow-md text-white animate-pulse">
                 <div className="h-6 bg-slate-800 rounded"></div>
                 <div className="h-2 mt-8 bg-slate-800 rounded"></div>
                 <div className="grid grid-cols-3 gap-4 mt-4">
-                  <div className="h-2 bg-slate-800 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-800 rounded col-span-1"></div>
+                  <div className="h-2 bg-slate-900 rounded col-span-2"></div>
+                  <div className="h-2 bg-slate-900 rounded col-span-1"></div>
                 </div>
                 <div className="h-2 mt-4 bg-slate-800 rounded"></div>
                 <div className="flex flex-row bg-slate-800 justify-between h-2 mt-10 my-4">
@@ -76,69 +83,20 @@ const Home = () => {
                   {new Date(Number(vault.deadline) * 1000).toLocaleString()}
                 </h1>
               </div>
-              <button className="min-w-full py-2  bg-slate-100 text-black hover:bg-slate-300  rounded-md">
+              <button className="min-w-full py-2  bg-slate-100 text-black hover:bg-purple-600 hover:text-white  rounded-md">
                 View Details
               </button>
             </div>
           </div>
         ))}
-        {/* <div className="px-7 py-7 bg-slate-950 rounded-lg shadow-md ">
-          <h1 className="text-2xl  font-bold text-white">Project Name 👀</h1>
-          <p className="text-sm py-4 text-white">
-            Project description will be writtten here. Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Dolore quae, nostrum deserunt
-          </p>
-          <div className="flex flex-row justify-between py-4">
-            <h1 className="text-base  text-white">Minimum limit: 56 ETH</h1>
-            <h1 className="text-base  text-white">Created: 8/5/2024</h1>
-          </div>
-          <button className="min-w-full py-2  bg-slate-100 text-black hover:bg-slate-300  rounded-md">
-            View Details
-          </button>
-        </div>
-        <div className="px-7 py-7  bg-slate-950 rounded-lg shadow-md ">
-          <h1 className="text-2xl  font-bold text-white">Project Name 👀</h1>
-          <p className="text-sm py-4 text-white">
-            Project description will be writtten here. Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Dolore quae, nostrum deserunt
-          </p>
-          <div className="flex flex-row justify-between py-4">
-            <h1 className="text-base  text-white">Minimum limit: 56 ETH</h1>
-            <h1 className="text-base  text-white">Created: 8/5/2024</h1>
-          </div>
-          <button className="min-w-full py-2  bg-slate-100 text-black hover:bg-slate-300  rounded-md">
-            View Details
-          </button>
-        </div>
-        <div className="px-7 py-7  bg-slate-950 rounded-lg shadow-md ">
-          <h1 className="text-2xl  font-bold text-white">Project Name 👀</h1>
-          <p className="text-sm py-4 text-white">
-            Project description will be writtten here. Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Dolore quae, nostrum deserunt
-          </p>
-          <div className="flex flex-row justify-between py-4">
-            <h1 className="text-base  text-white">Minimum limit: 56 ETH</h1>
-            <h1 className="text-base  text-white">Created: 8/5/2024</h1>
-          </div>
-          <button className="min-w-full py-2  bg-slate-100 text-black hover:bg-slate-300  rounded-md">
-            View Details
-          </button>
-        </div>
-        <div className="px-7 py-7  bg-slate-950 rounded-lg shadow-md ">
-          <h1 className="text-2xl  font-bold text-white">Project Name 👀</h1>
-          <p className="text-sm py-4 text-white">
-            Project description will be writtten here. Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Dolore quae, nostrum deserunt
-          </p>
-          <div className="flex flex-row justify-between py-4">
-            <h1 className="text-base  text-white">Minimum limit: 56 ETH</h1>
-            <h1 className="text-base  text-white">Created: 8/5/2024</h1>
-          </div>
-          <button className="min-w-full py-2  bg-slate-100 text-black hover:bg-slate-300  rounded-md">
-            View Details
-          </button>
-        </div> */}
       </div>
+      <a href="" target="_blank" rel="noopener noreferrer">
+        <div className="flex flex-auto align-center justify-center">
+          <button className=" text-white font-semibold rounded-md py-2 px-5 bg-purple-600 hover:bg-purple-700 ">
+            More Projects
+          </button>
+        </div>
+      </a>
     </div>
   );
 };
