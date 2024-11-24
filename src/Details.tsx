27967a575a8f2b057e8976e0cbc0d1d6e28bccb4
zain171m <zain171m@gmail.com>
2024-11-24 17:41:47 +0500

    updated Details page vault action tab

--- src/Details.tsx
+++ src/Details.tsx
@@ -30,14 +30,25 @@ const Details = () => {
   //const vaultDetails = response?.data as VaultDetailsType;
   const [activeTab, setActiveTab] = useState("Fund Project");
 
-  const tabs = ["Fund Project", "Refund", "Withdraw Funds", "Add CAT"];
   const account = useAccount();
   const nativecurrency = account.chain?.nativeCurrency.name;
+
+  const tabs = [
+    "Fund Project",
+    "Refund",
+    "Withdraw Funds",
+    "Add CAT",
+    "Withdraw Unsold CAT",
+  ];
+  const visibleTabs =
+    account.address === vaultDetails?.withdrawlAddress
+      ? tabs
+      : tabs.filter((tab) => tab === "Fund Project" || tab === "Refund");
 
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { isSubmitting },
   } = useForm<Inputs>();
   const onSubmitForm1: SubmitHandler<Inputs> = async (data) => {
     try {
@@ -46,6 +57,49 @@ const Details = () => {
         address: address as `0x${string}`,
         functionName: "purchaseTokens",
         value: parseEther(data.ethAmount),
+        chainId: arbitrumSepolia.id,
+      });
+      // Wait for approximately 6 seconds for 3 block confirmations
+      await new Promise((resolve) => setTimeout(resolve, 6000));
+      console.log("1st Transaction submitted:", tx1);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+  };
+
+  const {
+    register: register2,
+    handleSubmit: handleSubmit2,
+    formState: { isSubmitting: isSubmitting2 },
+  } = useForm<Inputs>();
+  const onSubmitForm2: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const tx1 = await writeContractAsync({
+        abi: vaultabi,
+        address: address as `0x${string}`,
+        functionName: "addTokens",
+        args: [parseEther(data.ethAmount)],
+        chainId: arbitrumSepolia.id,
+      });
+      // Wait for approximately 6 seconds for 3 block confirmations
+      await new Promise((resolve) => setTimeout(resolve, 6000));
+      console.log("1st Transaction submitted:", tx1);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+  };
+  const {
+    register: register3,
+    handleSubmit: handleSubmit3,
+    formState: { isSubmitting: isSubmitting3 },
+  } = useForm<Inputs>();
+  const onSubmitForm3: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const tx1 = await writeContractAsync({
+        abi: vaultabi,
+        address: address as `0x${string}`,
+        functionName: "withdrawUnsoldTokens",
+        args: [parseEther(data.ethAmount)],
         chainId: arbitrumSepolia.id,
       });
       // Wait for approximately 6 seconds for 3 block confirmations
@@ -366,7 +420,7 @@ const Details = () => {
         <div>
           <h1 className="text-2xl font-bold text-white">Vault Actions</h1>
           <div className="flex space-x-4 border-b pt-5">
-            {tabs.map((tab) => (
+            {visibleTabs.map((tab) => (
               <button
                 key={tab}
                 className={`px-4 py-2 ${
@@ -387,7 +441,7 @@ const Details = () => {
                   Fund the project and receive Participation tokens
                 </p>
                 <input
-                  className="input h-[34px]  text-[14px] text-white/60 w-1/3 bg-slate-950 text-[#f4f4f5] px-3 py-2 rounded-lg border border-white/10 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-[#09090b] transition-all duration-150 ease-in-out"
+                  className="input h-[34px]  text-[14px] text-white/60 w-1/3 bg-slate-950 text-[#f4f4f5] px-3 py-2 rounded border border-white/10 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-[#09090b] transition-all duration-150 ease-in-out"
                   type="number"
                   step="any"
                   {...register("ethAmount", { required: true })}
@@ -398,11 +452,16 @@ const Details = () => {
                   }
                   disabled={!nativecurrency}
                 />
-                <button className="flex h-[34px] min-w-60 overflow-hidden items-center font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-slate-950 text-white shadow hover:bg-black/90 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out  border-2 border-purple-600/70 hover:border-purple-600 mt-3">
+                <button
+                  disabled={!nativecurrency}
+                  className="flex h-[34px] min-w-60 overflow-hidden items-center font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-slate-950 text-white shadow hover:bg-black/90 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out  border-2 border-purple-600/70 hover:border-purple-600 mt-3"
+                >
                   <span className="absolute right-0 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-20 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
 
                   <span className="text-white">
-                    {isSubmitting ? "Processing..." : `Send ${nativecurrency}`}
+                    {nativecurrency
+                      ? `${isSubmitting ? "Processing..." : `Send ${nativecurrency}`}`
+                      : "Connect Wallet"}
                   </span>
                 </button>
               </form>
@@ -413,11 +472,16 @@ const Details = () => {
                   <p className="">
                     Skeptical about project Ligitimacy? Refund Your donations
                   </p>
-                  <button className="flex h-[34px] min-w-60 overflow-hidden items-center font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-slate-950 text-white shadow hover:bg-black/90 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out  border-2 border-purple-600/70 hover:border-purple-600 mt-3">
+                  <button
+                    disabled={!nativecurrency}
+                    className="flex h-[34px] min-w-60 overflow-hidden items-center font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-slate-950 text-white shadow hover:bg-black/90 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out  border-2 border-purple-600/70 hover:border-purple-600 mt-3"
+                  >
                     <span className="absolute right-0 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-20 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
 
                     <span className="text-white">
-                      {isSubmitting ? "Processing..." : `Withdraw Funds`}
+                      {nativecurrency
+                        ? `${isSubmitting ? "Processing..." : `Refund`}`
+                        : "Connect Wallet"}
                     </span>
                   </button>
                 </div>
@@ -436,13 +500,13 @@ const Details = () => {
               </div>
             )}
             {activeTab === "Add CAT" && (
-              <form onSubmit={handleSubmit(onSubmitForm1)}>
+              <form onSubmit={handleSubmit2(onSubmitForm2)}>
                 <p className="pb-5">Add more Contribution Accounting Tokens</p>
                 <input
-                  className="input h-[34px]  text-[14px] text-white/60 w-1/3 bg-slate-950 text-[#f4f4f5] px-3 py-2 rounded-lg border border-white/10 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-[#09090b] transition-all duration-150 ease-in-out"
+                  className="input h-[34px]  text-[14px] text-white/60 w-1/3 bg-slate-950 text-[#f4f4f5] px-3 py-2 rounded border border-white/10 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-[#09090b] transition-all duration-150 ease-in-out"
                   type="number"
                   step="any"
-                  {...register("ethAmount", { required: true })}
+                  {...register2("ethAmount", { required: true })}
                   placeholder={
                     nativecurrency
                       ? `Enter Amount of Tokens to add`
@@ -454,7 +518,31 @@ const Details = () => {
                   <span className="absolute right-0 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-20 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
 
                   <span className="text-white">
-                    {isSubmitting ? "Processing..." : `Send CATs`}
+                    {isSubmitting2 ? "Processing..." : `Add CATs`}
+                  </span>
+                </button>
+              </form>
+            )}
+            {activeTab === "Withdraw Unsold CAT" && (
+              <form onSubmit={handleSubmit3(onSubmitForm3)}>
+                <p className="pb-5">Withdraw Contribution Accounting Tokens</p>
+                <input
+                  className="input h-[34px]  text-[14px] text-white/60 w-1/3 bg-slate-950 text-[#f4f4f5] px-3 py-2 rounded border border-white/10 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-[#09090b] transition-all duration-150 ease-in-out"
+                  type="number"
+                  step="any"
+                  {...register3("ethAmount", { required: true })}
+                  placeholder={
+                    nativecurrency
+                      ? `Enter Amount of Tokens to Withdraw`
+                      : "Connect Wallet to proceed"
+                  }
+                  disabled={!nativecurrency}
+                />
+                <button className="flex h-[34px] min-w-60 overflow-hidden items-center font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-slate-950 text-white shadow hover:bg-black/90 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out  border-2 border-purple-600/70 hover:border-purple-600 mt-3">
+                  <span className="absolute right-0 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-20 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
+
+                  <span className="text-white">
+                    {isSubmitting3 ? "Processing..." : `Withdraw CATs`}
                   </span>
                 </button>
               </form>
